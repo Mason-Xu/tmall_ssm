1f8629afadd811985f0857444f10de8c0fd0a361
MasonXu <xulihao1997@gmail.com>
2020-03-17 12:29:03 +0800

    后台-分类管理 - 编辑功能 - 修改功能

--- src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
+++ src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
@@ -2,9 +2,9 @@ package com.how2java.tmall.mapper;
 
 import com.how2java.tmall.util.Page;
 import com.how2java.tmall.pojo.Category;
- 
+
 import java.util.List;
- 
+
 public interface CategoryMapper {
     List<Category> list(Page page);
 
@@ -13,4 +13,8 @@ public interface CategoryMapper {
     void add(Category category);
 
     void delete(int id);
+
+    Category get(int id);
+
+    void update(Category category);
 }
