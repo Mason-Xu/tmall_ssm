e79b0a430c635e66e90768a113730f73d20c5c45
MasonXu <xulihao1997@gmail.com>
2020-03-17 18:07:42 +0800

    重构 - 分页方式 使用PageHelper插件分页

--- src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
+++ src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
@@ -1,14 +1,11 @@
 package com.how2java.tmall.mapper;
 
-import com.how2java.tmall.util.Page;
 import com.how2java.tmall.pojo.Category;
 
 import java.util.List;
 
 public interface CategoryMapper {
-    List<Category> list(Page page);
-
-    int total();
+    List<Category> list();
 
     void add(Category category);
 
