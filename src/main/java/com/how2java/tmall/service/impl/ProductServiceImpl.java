acad045d3cce1bd46f9cf2b9fd6409a755ab36ca
MasonXu <xulihao1997@gmail.com>
2020-04-08 23:52:53 +0800

    前台-首页 - HOME方法

--- src/main/java/com/how2java/tmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/how2java/tmall/service/impl/ProductServiceImpl.java
@@ -11,6 +11,7 @@ import com.how2java.tmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -82,6 +83,38 @@ public class ProductServiceImpl implements ProductService {
         }
     }
 
+    //为分类填充产品集合
+    @Override
+    public void fill(Category c) {
+        List<Product> ps = list(c.getId());
+        c.setProducts(ps);
+    }
+
+    //为多个分类添加产品集合
+    @Override
+    public void fill(List<Category> cs) {
+        for (Category c : cs) {
+            fill(c);
+        }
+    }
+
+    //为多个分类填充推荐分类集合,即把分类下的产品集合,按照8个为一行,拆成多行.有利于显示在页面
+    @Override
+    public void fillByRow(List<Category> cs) {
+        int productNumberEachRow = 8;
+        for (Category c : cs) {
+            List<Product> products = c.getProducts();
+            List<List<Product>> productsByRow = new ArrayList<>();
+            for (int i = 0; i < products.size(); i += productNumberEachRow) {
+                int size = i + productNumberEachRow;
+                size = size > products.size() ? products.size() : size;
+                List<Product> productcOfEachRow = products.subList(i, size);
+                productsByRow.add(productcOfEachRow);
+            }
+            c.setProductByRow(productsByRow);
+        }
+    }
+
     //增加方法 setFirstProductImage(List<Product> ps)
     //给多个产品设置图片
     public void setFirstProductImage(List<Product> ps) {
@@ -89,4 +122,6 @@ public class ProductServiceImpl implements ProductService {
             setFirstProductImage(p);
         }
     }
+
+
 }
