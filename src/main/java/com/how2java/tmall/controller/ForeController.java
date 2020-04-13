307e36136f2712900a83e21003087d5e6bdf1e51
MasonXu <xulihao1997@gmail.com>
2020-04-13 13:12:56 +0800

    前台-无需登录 - 登陆

--- src/main/java/com/how2java/tmall/controller/ForeController.java
+++ src/main/java/com/how2java/tmall/controller/ForeController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.util.HtmlUtils;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -48,6 +50,7 @@ public class ForeController {
         if (exist) {
             String m = "用户名已经被使用,不能使用";
             model.addAttribute("msg", m);
+            //注册失败不会在header请登录显示user的注册名字
             model.addAttribute("user", null);
             return "fore/register";
         }
@@ -55,4 +58,19 @@ public class ForeController {
 
         return "redirect:registerSuccessPage";
     }
+
+    @RequestMapping("forelogin")
+    public String login(@RequestParam("name") String name, @RequestParam("password") String password, Model model, HttpSession session) {
+        name = HtmlUtils.htmlEscape(name);
+        User user = userService.get(name, password);
+
+        if (null == user) {
+            model.addAttribute("msg", "账号密码错误");
+            return "fore/login";
+        }
+        session.setAttribute("user", user);
+        return "redirect:forehome";
+    }
 }
+
+
---
+++ src/main/webapp/WEB-INF/jsp/fore/login.jsp
@@ -0,0 +1,13 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Ma5on
+  Date: 2020/4/12
+  Time: 10:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html; charset=UTF-8" language="java"
+         pageEncoding="UTF-8" isELIgnored="false" %>
+
+<%@include file="../include/fore/header.jsp" %>
+<%@include file="../include/fore/loginPage.jsp" %>
+<%@include file="../include/fore/footer.jsp" %>
